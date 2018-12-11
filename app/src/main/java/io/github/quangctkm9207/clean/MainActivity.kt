66027ac0eb127629bd0fb1dc436ed27e4fb3091a
Quang Nguyen <quangctkm9207@gmail.com>
2018-12-11 09:35:18 +0900

    Inital commit

---
+++ app/src/main/java/io/github/quangctkm9207/clean/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.quangctkm9207.clean
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ domain/src/main/java/io/github/quangctkm9207/clean/domain/UseCase.java
@@ -0,0 +1,4 @@
+package io.github.quangctkm9207.clean.domain;
+
+public class UseCase {
+}
